9d61b540cf27f51a2342ca83b3b8215328f6a0ec
Tiqi414 <huhuyu0033@gmail.com>
2026-06-09 22:03:07 +0800

    feat: 新增类型注解和模块的学习

---
+++ 第二章/my_module/circle_fun.py
@@ -0,0 +1,9 @@
+# 根据半径计算圆的面积
+def circle_area(r):
+    pi = 3.14
+    return round(pi * (r ** 2), 2)
+
+# 根据圆的半径计算圆的周长
+def circle_len(r):
+    pi = 3.14
+    return round(pi * r * 2, 2)
---
+++ 第二章/utils/__init__.py
@@ -0,0 +1,5 @@
+# 描述
+__version__ = "1.0.0"
+__author__ = "呼呼"
+
+__all__ = ["my_fun", "my_var"]
